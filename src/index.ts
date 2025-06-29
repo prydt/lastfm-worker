3456e79d1e47579b4372391d57eb369ae1523169
prydt <pranoydll@gmail.com>
2025-06-29 17:50:50 -0500

    add caching to avoid hitting rate limit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+
 const app = new Hono();
 
 function lastfm_url(method: String, name: String, api_key: String) {
@@ -10,8 +11,19 @@ app.get('/', (c) => c.text("prydt's lastfm cloudflare worker"));
 app.get('/user/:name', async (c) => {
 	const name = c.req.param('name');
 	const key = c.env.LASTFM_API_KEY;
+	const url = lastfm_url('user.getRecentTracks', name, key)
 
-	let response = await fetch(lastfm_url('user.getRecentTracks', name, key));
+	let response = await fetch(url, {
+		cf: {
+			cacheEverything: true,
+			cacheTtl: 21600, // max cache duration of 6 hours
+			cacheKey: url,
+		}
+	});
+
+	// set browser cache header to cache for 12 hours
+	response = new Response(response.body, response)
+	response.headers.set('Cache-Control', 'max-age=43200')
 
 	return response;
 });
