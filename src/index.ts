9426d79bcd58362fd85b1460d278befc56ee1d72
prydt <pranoydll@gmail.com>
2025-06-29 18:31:07 -0500

    Add rate limiting
    Mostly so that my API key doesn't get abused. this limit should be quite hard to hit.
    
    This should really not be a problem unless someone loops through like a million different users (since each new one will be uncached) and doing this very quickly.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono';
+import { Bindings } from 'hono/types';
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Env }>();
 
 function lastfm_url(method: String, name: String, api_key: String) {
 	return `http://ws.audioscrobbler.com/2.0/?method=${method}&user=${name}&api_key=${api_key}&format=json`;
@@ -11,19 +12,30 @@ app.get('/', (c) => c.text("prydt's lastfm cloudflare worker"));
 app.get('/user/:name', async (c) => {
 	const name = c.req.param('name');
 	const key = c.env.LASTFM_API_KEY;
-	const url = lastfm_url('user.getRecentTracks', name, key)
+	const url = lastfm_url('user.getRecentTracks', name, key);
+	const ipaddr = c.req.header('cf-connecting-ip') || '';
+
+	// Generally you shouldn't rate limit based on IP due to IP sharing,
+	// but we really shouldn't ever hit this limit.
+	// This is a better option than stopping every single person from accessing
+	// the API rather than just potentially a single mobile service provider or something.
+	const { success } = await c.env.RATE_LIMITER.limit({ key: ipaddr });
+
+	if (!success) {
+		return new Response('API rate limit exceeded', { status: 429 });
+	}
 
 	let response = await fetch(url, {
 		cf: {
 			cacheEverything: true,
 			cacheTtl: 21600, // max cache duration of 6 hours
 			cacheKey: url,
-		}
+		},
 	});
 
 	// set browser cache header to cache for 12 hours
-	response = new Response(response.body, response)
-	response.headers.set('Cache-Control', 'max-age=43200')
+	response = new Response(response.body, response);
+	response.headers.set('Cache-Control', 'max-age=43200');
 
 	return response;
 });
